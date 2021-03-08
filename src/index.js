44cf69f7dcb41c39933978be6c9ab7d74311b04b
fanyu1738140 <fany23@uw.edu>
2021-03-07 23:53:02 -0800

    fullpage test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ new fullpage('#fullPage', {
         licenseKey: '3B260588-D40B40CC-9E84CF5D-1BAC5B3D',
         autoScrolling: true,
         navigation: true,
-        navigationTooltips: ['page 1', 'page 2', 'page 3', 'page 4'],
-        showActiveTooltip: true,
         loopBottom: true
     })
